48930764a04eec0fe2b8e6cd47272f68948042bf
Nilesh Keshari <kesharinilesh13@gmail.com>
2023-10-01 16:54:02 +0530

    mention clients

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ var tl=gsap.timeline({
     scrollTrigger:{trigger:".page1 h1",
     scroller:".main",
     markers:true,
-    start:"top 37%",
-    end:"top 0",
+    start:"top 20%",
+    end:"top -25%",
     scrub:3
     }    
 })
@@ -127,3 +127,38 @@ tl3.to(".page3-part2 #seemore h5",{
 tl3.to(".page4",{
     backgroundColor: "#0F0D0D",
 },"anim")
+
+
+
+var boxes = document.querySelectorAll(".box")
+boxes.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(dets){
+        var att = elem.getAttribute("data-image")
+        crsr.style.width = "470px"
+        crsr.style.height = "370px"
+        crsr.style.borderRadius = "0"
+        crsr.style.backgroundImage = `url(${att})`
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.style.backgroundColor = "transparent"
+        crsr.style.width = "20px"
+        crsr.style.height = "20px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none`
+    })
+})
+
+var h4 = document.querySelectorAll("#nav h4")
+var purple = document.querySelector("#purple")
+h4.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        purple.style.display = "block"   
+        purple.style.opacity = "1"
+        crsr.style.backgroundColor="white"
+    })
+    elem.addEventListener("mouseleave",function(){
+        purple.style.display = "none"   
+        purple.style.opacity = "0"
+        crsr.style.backgroundColor="#edbfff"
+    })
+})
